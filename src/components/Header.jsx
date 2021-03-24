3d455efa106adcfa1b968262150fe7907a88e673
Sergey <sergeyj12@gmail.com>
2021-03-24 23:06:27 +0200

    initial

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+const Header = () => {
+    return  <header className='header'>
+        <img src="https://img1.freepng.ru/20171220/gze/twitter-logo-png-5a3a1851372e76.0876249315137567532269680.jpg"/>
+      </header>
+}
+
+export default Header
---
+++ src/components/Profile.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+
+const Profile = () => {
+    return  <div className='content'>
+    <div>
+    <img height="300px" src='https://imgcomfort.com/Userfiles/Upload/images/illustration-geiranger.jpg' />
+    </div>
+    <div>
+      my posts
+      <div>
+        New post
+      </div>
+      <div>
+        post1
+      </div>
+      <div>
+        post2
+      </div>
+    </div>
+  </div>
+}
+
+export default Profile
