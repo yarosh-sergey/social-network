c9f81823217eedac7010c89d3fd8341ab2800fbc
Sergey <sergeyj12@gmail.com>
2021-03-26 20:05:16 +0200

    add modules to css files

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,22 @@
 import React from 'react'
+import classes from './Navbar.module.css'
 
 
 const Navbar = () => {
-    return  <nav className='nav'>
-    <div className="item">
+    return  <nav className={classes.nav}>
+    <div className={classes.item}>
       <a>Profile</a>
     </div>
-    <div className="item">
+    <div className={classes.item}>
       <a>Messages</a>
     </div>
-    <div className="item">
+    <div className={classes.item}>
       <a>News</a>
     </div>
-    <div className="item">
+    <div className={classes.item}>
       <a>Music</a>
     </div>
-    <div className="item" >
+    <div className={classes.item}>
       <a>Settings</a>
     </div>
   </nav>
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,20 +1,21 @@
 import React from 'react'
+import classes from './Profile.module.css'
 
 
 const Profile = () => {
-    return  <div className='content'>
+    return  <div className={classes.content}>
     <div>
     <img height="300px" src='https://imgcomfort.com/Userfiles/Upload/images/illustration-geiranger.jpg' />
     </div>
-    <div>
+    <div className={classes.posts}>
       my posts
-      <div>
+      <div className={classes.item}>
         New post
       </div>
-      <div>
+      <div className={classes.item}>
         post1
       </div>
-      <div>
+      <div className={classes.item}>
         post2
       </div>
     </div>
