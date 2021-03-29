eecef691fcc937362e923ee1f4ec1348c4322d2b
Sergey <sergeyj12@gmail.com>
2021-03-29 23:16:53 +0300

    added props

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -5,8 +5,8 @@ import Post from "./Post/Post";
 const MyPosts = () => {
   return (
     <div className={classes.posts}>
-      <Post />
-      <Post />
+      <Post message='Hi, how are you?' />
+      <Post message='It`s my first post!' />
       <Post />
       <Post />
     </div>
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,15 +1,19 @@
-import React from 'react'
-import MyPosts from './MyPosts/MyPosts'
-import classes from './Profile.module.css'
-
+import React from "react";
+import MyPosts from "./MyPosts/MyPosts";
+import classes from "./Profile.module.css";
 
 const Profile = () => {
-    return  <div className={classes.content}>
-    <div>
-    <img height="300px" src='https://imgcomfort.com/Userfiles/Upload/images/illustration-geiranger.jpg' />
+  return (
+    <div className={classes.content}>
+      <div>
+        <img
+          height="300px"
+          src="https://imgcomfort.com/Userfiles/Upload/images/illustration-geiranger.jpg"
+        />
+      </div>
+      <MyPosts />
     </div>
-    <MyPosts />
-    </div>
-}
+  );
+};
 
-export default Profile
+export default Profile;
